eae00150883b0b82bb13301196611cbd853c5bd2
unknown <zammad658@gmail.com>
2021-06-27 16:51:19 +0500

    first code

--- app/Jobs/ExcelRecordJob.php
+++ app/Jobs/ExcelRecordJob.php
@@ -2,7 +2,6 @@
 
 namespace App\Jobs;
 
-use App\Model\Data;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -18,12 +17,9 @@ class ExcelRecordJob implements ShouldQueue
      *
      * @return void
      */
-    public $data;
-    public $file_id;
-    public function __construct(array $record,$file_id)
+    public function __construct()
     {
-        $this->file_id = $file_id;
-        $this->data = $record;
+        //
     }
 
     /**
@@ -33,28 +29,6 @@ class ExcelRecordJob implements ShouldQueue
      */
     public function handle()
     {
-        if(isset($this->data) && !empty($this->data))
-        {
-            foreach($this->data as $key => $row)
-            {
-                $insert_data[] = array(
-                    'file_id'  => $this->file_id,
-                    'name'   => $row['name'],
-                    'roll_number'   => $row['roll_number'],
-                    'tester'    => $row['tester'],
-                );
-            }
-            if(!empty($insert_data))
-            {
-                foreach ($insert_data as $data){
-                    $store = new Data();
-                    $store->file_id = $data['file_id'];
-                    $store->name = $data['name'];
-                    $store->roll_number = $data['roll_number'];
-                    $store->tester = $data['tester'];
-                    $store->save();
-                }
-            }
-        }
+        //
     }
 }
--- app/Services/ImportExcel.php
+++ app/Services/ImportExcel.php
@@ -5,7 +5,7 @@ namespace App\Services;
 
 
 use App\Http\Requests\ImportExcelRequest;
-use App\Jobs\ExcelRecordJob;
+use App\Model\Data;
 use App\Model\File;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -23,11 +23,36 @@ class ImportExcel
             $file->original_name = time().'_'.$original_path;
             $file->save();
 
+
             $record = Excel::load($path)->get();
-            if ($record){
-                ExcelRecordJob::dispatch($record->toArray(),$file->id);
+
+            if($record->count() > 0)
+            {
+                foreach($record->toArray() as $key => $row)
+                {
+                        $insert_data[] = array(
+                            'file_id'  => $file->id,
+                            'name'   => $row['name'],
+                            'roll_number'   => $row['roll_number'],
+                            'tester'    => $row['tester'],
+                        );
+//                        dd($insert_data);
+
+                }
+//                dd($insert_data);
+                if(!empty($insert_data))
+                {
+                    foreach ($insert_data as $data){
+                        $store = new Data();
+                        $store->file_id = $data['file_id'];
+                        $store->name = $data['name'];
+                        $store->roll_number = $data['roll_number'];
+                        $store->tester = $data['tester'];
+                        $store->save();
+                    }
+                }
+                return back()->with('success', 'Excel Data Imported successfully.');
             }
-            return redirect()->route('index')->with('success', 'your file is being imported. we will inform you once it is done.');
         }catch(\Exception $e){
             return redirect()->back()->with('error', error_details($e,'Something went wrong!'.$e->getMessage()));
         }
