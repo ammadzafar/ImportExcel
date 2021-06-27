b7ef417336dd160c0cd618e743730824adf8b8b6
unknown <zammad658@gmail.com>
2021-06-27 16:44:25 +0500

    with queue job

--- app/Jobs/ExcelRecordJob.php
+++ app/Jobs/ExcelRecordJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Model\Data;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -17,9 +18,12 @@ class ExcelRecordJob implements ShouldQueue
      *
      * @return void
      */
-    public function __construct()
+    public $data;
+    public $file_id;
+    public function __construct(array $record,$file_id)
     {
-        //
+        $this->file_id = $file_id;
+        $this->data = $record;
     }
 
     /**
@@ -29,6 +33,28 @@ class ExcelRecordJob implements ShouldQueue
      */
     public function handle()
     {
-        //
+        if(isset($this->data) && !empty($this->data))
+        {
+            foreach($this->data as $key => $row)
+            {
+                $insert_data[] = array(
+                    'file_id'  => $this->file_id,
+                    'name'   => $row['name'],
+                    'roll_number'   => $row['roll_number'],
+                    'tester'    => $row['tester'],
+                );
+            }
+            if(!empty($insert_data))
+            {
+                foreach ($insert_data as $data){
+                    $store = new Data();
+                    $store->file_id = $data['file_id'];
+                    $store->name = $data['name'];
+                    $store->roll_number = $data['roll_number'];
+                    $store->tester = $data['tester'];
+                    $store->save();
+                }
+            }
+        }
     }
 }
